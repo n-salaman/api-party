349ab20399ffc9a6908e9e4dddda7b08d3364d30
Noah Salaman <nsalaman@iu.edu>
2018-06-19 10:53:32 -0400

    added some css for pokemon images

--- src/PokemonSingle.js
+++ src/PokemonSingle.js
@@ -30,7 +30,7 @@ class PokemonSingle extends Component {
     return (
       <div className="PokemonUser">
         <h3>{pokemon.name}</h3>
-        <img src = {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} />
+        <img className="PokemonImg" src = {`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.id}.png`} />
       </div>
     )
   }
